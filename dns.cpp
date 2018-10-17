9785391964198c8ff3d0f648c0d46835fb6b34bd
mahonghui <amisher@163.com>
2018-10-17 20:02:59 +0800

    define dns server workflow

---
+++ dns.cpp
@@ -0,0 +1,151 @@
+/*
+	brief: dns section implement
+*/
+
+#include "dns.h"
+#include <iostream>
+#include <sys/socket.h>
+#include <sys/types>
+#include <arpa/inet.h>
+#include <sstream>
+#include <cstdlib>
+
+// constructor & deconstructor
+CDns::CDns(const char* filename)
+	:m_socket(0),
+	m_clientaddr(),
+	m_error(false),
+	m_log(filename),
+	m_db()
+	{}
+
+CDns::~CDns(){}
+
+void CDns::openCommunication()
+{
+	struct sockaddr_in server_address;
+	int length;
+
+	// create a socket
+	m_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	if(m_socket < 0)
+	{
+		cerr<<"Fail to open socket"<<endl;
+		exit(0);
+	}
+
+	// turn on reuse of socket
+	int op = 1;
+	setsockopt(m_socket, SOL_SOCKET, SO_REUSEADDR, &op, sizeof(op));
+
+	// initialize address
+	length = sizeof(server_address);
+	memset(&server_address, 0, length);
+	server_address.sin_family = AF_INET;
+	server_address.sin_address.s_addr = htonl(INADDR_ANY);
+	server_address.sin_port = htons(DNS_PORT);
+
+	if(bind(m_socket, (struct sockaddr*)&server_address, length)<0)
+	{
+		cerr<<"Error binding socket"<<endl;
+		exit(0);
+	}
+
+
+	bool error = m_db.readfile("hosts");
+	if(error)
+	{
+		cerr<<"Error reading <hosts> file. File does not exist! "<<endl;
+		exit(0);
+	}
+
+	m_log.printString("Starting Name Server ...");
+}
+
+void CDns::readMessage()
+{
+	socklen_t len = sizeof(struct sockaddr_in);
+	int n;
+	int buff[1024];
+
+	// receive message
+	n = recvfrom(m_socket, (void*)buff, 1024, 0, (struct sockaddr*)&m_clientaddr,&len);
+	if(n<0)
+	{
+		cerr<<"Error receiving from "<<m_socket<<" socket"<<endl;
+		exit(0);
+	})
+
+	string received_msg((const char*) buff, n);
+	parseMessage(received_msg, (unsigned int) n);
+}
+
+void CDns::parseMessage(string& msg, unsigned int n)
+{
+	m_message = new CMessage(m_log);
+	m_error = false;
+
+	ostringstream s;
+	s<<DNS_PORT;
+	m_log.printString("Message receive from port: "+ s.c_str() + ". ");
+	m_log.printString("message: ");
+	m_log.printFormattedString(msg);
+
+	string header(msg, 0, HEADER_SIZE);
+	m_error = m_message->setHeader(header);
+	if(m_error)
+	{
+		m_log.printString("Error parsing message");
+
+		buildMessage(msg);
+		return;
+	}
+	hostLookup(msg);
+
+} 
+
+void CDns::hostLookup(string& msg)
+{
+	struct in_addr addr;
+	unsigned long saddr;
+	string hostname;
+
+	hostname = m_message->getHost();
+	addr.s_addr = m_db.getAddress(hostname);
+	saddr = htonl(addr.s_addr);
+
+	m_error = m_message->setAnswer(saddr);
+	if(m_error)
+	{
+		m_log.printString("hostlookup: no ip found");
+	}
+
+	buildMessage(msg);
+}
+
+void CDns::buildMessage(string& msg)
+{
+	m_message->getHeader(msg);
+
+	if(!m_error)
+	{
+		msg += m_message->getAnswer();
+		msg += m_message->getAuthority();
+		msg += m_message->getAddtional();	
+	}
+	sendMessage(msg);
+}
+
+void CDns::sendMessage(string& msg)
+{
+	int r;
+	socklen_t len = sizeof(struct sockaddr_in);
+
+	n = send(m_socket, msg.c_str(), msg.size(), 0, (struct sockaddr*)&m_clientaddr, len);
+	if(n<0)
+	{
+		cerr<<"Error sending to "<<m_clientaddr<<" socket."<<endl;
+		exit(0);
+	}
+	
+}
---
+++ dns.h
@@ -0,0 +1,46 @@
+/*
+	brief: define dns server workflow
+*/
+using namespace std;
+
+#ifndef _DNS_H
+#define _DNS_H
+
+#include "log"
+#include "message.h"
+#include "dnsDB.cpp"
+
+class CDns
+{
+public:
+	CDns(const char* filename);
+	~CDns();
+	
+	// open communication
+	void openCommunication();
+
+	// read message from client
+	void readMessage();
+
+	void parseMessage(string& txMessage, unsigned int mlen);
+
+	// look up ip address in the db
+	void hostLookup(string& txMessage);
+
+	void buildMessage(string& txMessage);
+
+	void sendMessage(string& txMessage);
+
+private:
+	static const unsigned short DNS_PORT = 53;
+	static const unsigned short HEADER_SIZE = 12;
+
+	int		m_socket;
+	struct sockaddr_in m_clientaddr;
+	bool		m_error;
+	CMessage*	m_message;
+	CLog		m_log;
+	CDnsDB		m_db;
+};
+
+#endif
